62ea91df9abef105203e54e7ff066922e5199944
Sanchez711 <106490860+Sanchez711@users.noreply.github.com>
2022-07-21 12:46:38 +0800

    Update bilibili.js

--- JS/bilibili.js
+++ JS/bilibili.js
@@ -1,23 +1,9 @@
-let body = JSON.parse($response.body);
-body['data']['official_verify']['type'] = 1;
-body['data']['official_verify']['desc'] = "管理员";
-body['data']['vip_type'] = 2;
-body['data']['vip_section_v2'] = {};
-body['data']['vip_section'] = {};
-body['data']['vip'] = {
-    "status": 1,
-    "avatar_subscript": 1,
-    "nickname_color": "#FB7299",
-    "due_date": 1964448000,
-    "role": 1,
-    "vip_pay_type": 0,
-    "avatar_subscript_url": "http:\/\/i0.hdslb.com\/bfs\/vip\/icon_Certification_big_member_22_3x.png",
-    "label": {
-        "bg_color": "#FB7299",
-        "bg_style": 1,
-        "path": "",
-        "text": "十年大会员",
-        "label_theme": "ten_annual_vip"
-    },
-    "type": 2,
-    "theme_type": 0
+# > 哔哩哔哩
+# >>  “我的信息”的修改
+^https?:\/\/.+\.(bilibili|biliapi)\.(com|net)\/x\/v2\/account\/myinfo\?access_key url script-response-body https://hub.qste.com/Script/Bilibili/MyInfo.js
+
+# >>  “空间”的修改
+^https?:\/\/.+\.(bilibili|biliapi)\.(com|net)\/x\/v2\/space\?access_key url script-response-body https://hub.qste.com/Script/Bilibili/Space.js
+
+# >>  “大会员”的修改
+^https?:\/\/.+\.(bilibili|biliapi)\.(com|net)\/x\/vip\/web\/vip_center\/combine url script-response-body https://hub.qste.com/Script/Bilibili/Center.js
