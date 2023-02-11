cd1bb781f751de1254c67ed4b0c48ca9d5178f81
Sanchez711 <106490860+Sanchez711@users.noreply.github.com>
2023-02-12 04:59:50 +0800

    Update bilibili.js

--- JS/bilibili.js
+++ JS/bilibili.js
@@ -1,8 +1,8 @@
 # > 哔哩哔哩
 # >>  “我的信息”的修改
-^https?:\/\/.+\.(bilibili|biliapi)\.(com|net)\/x\/v2\/account\/myinfo\?access_key url script-response-body https://https://raw.githubusercontent.com/Sanchez711/QuantumuitX/main/JS/MyInfo.js
+^https?:\/\/.+\.(bilibili|biliapi)\.(com|net)\/x\/v2\/account\/myinfo\?access_key url script-response-body https://raw.githubusercontent.com/Sanchez711/QuantumuitX/main/JS/MyInfo.js
 # >>  “空间”的修改
-^https?:\/\/.+\.(bilibili|biliapi)\.(com|net)\/x\/v2\/space\?access_key url script-response-body https://https://raw.githubusercontent.com/Sanchez711/QuantumuitX/main/JS/Space.js
+^https?:\/\/.+\.(bilibili|biliapi)\.(com|net)\/x\/v2\/space\?access_key url script-response-body https://raw.githubusercontent.com/Sanchez711/QuantumuitX/main/JS/Space.js
 
 # >>  “大会员”的修改
 ^https?:\/\/.+\.(bilibili|biliapi)\.(com|net)\/x\/vip\/web\/vip_center\/combine url script-response-body https://raw.githubusercontent.com/Sanchez711/QuantumuitX/main/JS/Center.js
