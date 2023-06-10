67c3ee1a8340aa48b1610920adc052e6c301f5bc
Squanchy <106490860+Changrete@users.noreply.github.com>
2023-06-11 02:04:43 +0800

    更新 i4.js

--- Rewrite/i4.js
+++
@@ -1,9 +0,0 @@
-[rewrite_local]
-
-
-#爱思
-^https:\/\/list-app-m\.i4\.cn\/getopfstadinfo\.xhtml url reject-200
-^https:\/\/list-app-m\.i4\.cn\/getopfstadinfo\.xhtml url reject-200
-
-
-hostname = list-app-m.i4.cn
