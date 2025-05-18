37b1f51321213692335704249cb47d5d16fd907f
Squanchy <106490860+Changrete@users.noreply.github.com>
2025-05-19 02:47:38 +0800

    更新 ipwho-de.js

--- ipwho-de.js
+++ ipwho-de.js
@@ -18,7 +18,7 @@ var cityName = obj['city_name'] || obj['country_name'];
 var countryFlag = flags.get(obj['country_code']) || '🌐';
 
 var title = countryFlag + ' ' + obj['country_code'] + '⋅' + cityName;
-#var subtitle = asName + '｜' + obj['ip'] + '⬩' + 'AS' + obj['asn'];
+var subtitle =  obj['ip'] + '⬩' + 'AS' + obj['asn'];
 var ip = obj['ip'];
 var description = countryFlag + '' + obj['country_name'] + '\n' + cityName + '\n' + obj['as_desc'] + '\n' + obj['ip'] + '\n' + 'AS' + obj['asn'];
 
